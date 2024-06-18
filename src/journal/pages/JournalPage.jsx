2ccd70347e790e9921861d5a27bf2bd3a1dcb91d
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-06-18 01:26:01 -0500

    Floating Button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
 
@@ -8,11 +9,25 @@ export const JournalPage = () => {
     <JournalLayout>
       {/* <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam aliquid consequuntur quae illum possimus voluptate blanditiis quam aspernatur tempore nulla, similique, necessitatibus, et officia nobis vitae voluptatem tenetur voluptatum architecto!</Typography> */}
 
-      {/* <NothingSelectedView /> */}
-      <NoteView />
-
       {/* Nothing Selected */}
+      <NothingSelectedView />
+
       {/* NoteView */}
+      {/* <NoteView /> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };

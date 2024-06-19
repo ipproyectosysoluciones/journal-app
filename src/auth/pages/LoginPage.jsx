558e73ab367f47bfdc1cfbe72b6fc988313807d5
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-06-19 10:45:46 -0500

    Trigger authentication action

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Google } from '@mui/icons-material';
@@ -10,12 +11,15 @@ import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
 
 export const LoginPage = () => {
 
-  const dispatch = useDispatch();
+  const { status } = useSelector( state => state.auth );
 
+  const dispatch = useDispatch();
   const { email, password, onInputChange } = useForm({
     email: 'johnDoe@google.com',
     password: '123456',
   });
+
+  const isAuthenticating = useMemo( () => status === 'checking', [ status ] );
 
   const onSubmit = ( event ) => {
     event .preventDefault();
@@ -58,6 +62,7 @@ export const LoginPage = () => {
             <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
               <Grid item xs={12} sm={6}>
                 <Button 
+                  disabled={ isAuthenticating }
                   type="submit" 
                   variant="contained" 
                   fullWidth
@@ -67,6 +72,7 @@ export const LoginPage = () => {
               </Grid>
               <Grid item xs={12} sm={6}>
                 <Button 
+                  disabled={ isAuthenticating }
                   variant="contained" 
                   fullWidth
                   onClick={ omGoogleSignIn }

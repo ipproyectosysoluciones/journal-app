cd6556cfb8ca5b0965db1a80ca663d7d6a7ec6fb
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-06-19 13:25:31 -0500

    User registration form - Form error handling - Validate from the custom hook

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,12 +1,37 @@
-import { Link as RouterLink } from "react-router-dom";
-import { Button, Grid, Link, TextField, Typography } from "@mui/material";
-import { Google } from "@mui/icons-material";
-import { AuthLayout } from "../layout/AuthLayout";
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks/useForm';
+
+const formData = {
+  displayName: 'John Doe',
+  email: 'johndoe@google.com',
+  password: '123456',
+}
+
+const formValidations = {
+  displayName: [(value) => value.trim().length > 1, 'Full name is required'],
+  email: [(value) => value.includes('@'), 'The email must have an @'],
+  password: [(value) => value.length >= 6, 'Password must be at least 6 characters long'],
+};
 
 export const RegisterPage = () => {
+
+  const { 
+    formState, displayName, email, password, onInputChange, 
+    isFormValid, displayNameValid, emailValid, passwordValid
+  } = useForm( formData, formValidations );
+
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+
+    console.log({ formState });
+  };
+
+
   return (
     <AuthLayout title="Crear Cuenta">
-      <form>
+      <form onSubmit={ onSubmit }>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -14,6 +39,11 @@ export const RegisterPage = () => {
               type="text"
               placeholder="Enter your full name"
               fullWidth
+              name="displayName"
+              value={ displayName }
+              onChange={ onInputChange }
+              error={ !!displayNameValid }
+              helperText={ displayNameValid }
             />
           </Grid>
 
@@ -23,6 +53,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="email@google.com"
               fullWidth
+              name="email"
+              value={ email }
+              onChange={ onInputChange }
             />
           </Grid>
 
@@ -32,11 +65,18 @@ export const RegisterPage = () => {
               type="password"
               placeholder="Contraseña"
               fullWidth
+              name="password"
+              value={ password }
+              onChange={ onInputChange }
             />
 
             <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
               <Grid item xs={12}>
-                <Button variant="contained" fullWidth>
+                <Button 
+                  type="submit"
+                  variant="contained" 
+                  fullWidth
+                >
                   Create Account
                 </Button>
               </Grid>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,8 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
+export const useForm = ( initialForm = {}, formValidations = {} ) => {
   
   const [ formState, setFormState ] = useState( initialForm );
+  const [ formValidation, setFormValidation ] = useState({});
+
+  useEffect(() => {
+    createValidators();
+  }, [ formState ]);
 
   const onInputChange = ({ target }) => {
     const { name, value } = target;
@@ -17,10 +22,22 @@ export const useForm = ( initialForm = {} ) => {
     setFormState( initialForm );
   };
 
+  const createValidators = () => {
+    const formCheckedValues = {};
+
+    for (const formField of Object.keys( formValidations )) {
+      const [ fn, errorMessage ] = formValidations[ formField ];
+      formCheckedValues[ `${formField}Valid` ] = fn( formState[ formField ] ) ? null : errorMessage;
+    }
+
+    setFormValidation( formCheckedValues );
+  };
+
   return {
     ...formState, 
     formState, 
     onInputChange, 
     onResetForm, 
+    ...formValidation,
   };
 };

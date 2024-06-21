c8a32be52681dddbdf7d366fe21626a1211d2571
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-06-21 03:50:53 -0500

    Animations for the application

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -13,6 +13,7 @@ export const NoteView = () => {
       sx={{
         mb: 1
       }}
+      className='animate__animated animate__fadeIn animate__faster'
     >
       <Grid item>
         <Typography fontSize={ 39 } fontWeight='Light'>June 18, 2024</Typography>
